7beacd0a6fc35ce0a774e321820ba2c56199858b
Alexandre Abreu <alexandre_1605@outlook.com>
2020-04-23 19:52:57 -0300

    Definindo objeto do produto, criando array de produtos para inserçao no banco

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const request = require('request');
 const cheerio = require('cheerio');
+const moment = require('moment');
+const Product = require('./model/product');
 
-let pichau = [];
+let Products = [];
 
 request('https://www.pichau.com.br/hardware/memorias?product_list_order=price&tipo_de_memoria=422', function(err, res, body) {
     let $ = cheerio.load(body);
 
     $('.product-items li').each(function() {
         $(this).find('.product-item .product-item-info').each(function() {
-            let product = $(this).find('.product-item-details .product-item-name a').text().trim();
+            let name = $(this).find('.product-item-details .product-item-name a').text().trim();
             let price = $(this).find('.product-item-details .price-final_price .price-boleto span').text().trim();
-            console.log(product + ' - ' + price);
+            let product = new Product(
+                name,
+                price,
+                'Pichau',
+                moment().format('DD/MM/YYYY'));
+            Products.push(product);
         });
     });
+    console.log(Products);
 });
---
+++ model/product.js
@@ -0,0 +1,8 @@
+let Product = function(name, price, seller, date) {
+    this.name = name;
+    this.price = price;
+    this.seller = seller;
+    this.date = date;
+};
+
+module.exports = Product;
