ce7cba47e9d93bdb74d1f52cb4de8b907f215788
Alexandre Abreu <alexandre_1605@outlook.com>
2020-05-18 15:07:32 -0300

    Correção na string de conexão

--- connection.js
+++ connection.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 const config = require('./config');
 
-const connect = new Sequelize(config.config.environmentironment.DB_NAME, config.environment.DB_USER, config.environment.DB_PASSWORD, {
+const connect = new Sequelize(config.environment.DB_NAME, config.environment.DB_USER, config.environment.DB_PASSWORD, {
     host: config.environment.DB_ADDRESS,
     dialect: 'mysql',
     port: config.environment.DB_PORT
